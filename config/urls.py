54d47f894b63209035af5191b99dcbc019d4a3d6
souhmahma <souhailhmahma@gmail.com>
2026-03-24 14:33:56 +0100

    dashboard

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/',        admin.site.urls),
-    path('api/auth/',     include('accounts.urls')),
-    path('api/shops/',    include('shops.urls')),
-    path('api/products/', include('products.urls')),
-    path('api/orders/',   include('orders.urls')),
+    path('admin/',       admin.site.urls),
+    path('api/auth/',    include('accounts.urls')),
+    path('api/shops/',   include('shops.urls')),
+    path('api/products/',include('products.urls')),
+    path('api/orders/',  include('orders.urls')),
+    path('api/dashboard/',include('dashboard.urls')),  # 🆕
 ]
